916926c61202551c98dfb8c7deda3ec27222a678
Rudran Chakraborty <rudranchakraborty@mac.code>
2025-08-24 21:27:21 +0200

    feat: mc fetch rvfi test

--- cores/sparrowx32/wrapper.sv
+++ cores/sparrowx32/wrapper.sv
@@ -5,70 +5,62 @@ module rvfi_wrapper (
 );
 
 	(* keep *) reg                        pil_run_prg = 0;
-	(* keep *) `rvformal_rand_reg [31:0]  piv_inst;
-	(* keep *) wire               [31:0]  pov_addr;
+	
+	// (* keep *) `rvformal_rand_reg         pil_fetch_mem_valid;
+	// (* keep *) `rvformal_rand_reg         pil_fetch_mem_ack;
+	(* keep *) reg                        pil_fetch_mem_valid = 0;
+	(* keep *) reg                        pil_fetch_mem_ack = 0;
+	(* keep *) wire                       pol_fetch_mem_req;
+	(* keep *) `rvformal_rand_reg [31:0]  piv_fetch_mem_rdata;
+	(* keep *) wire               [31:0]  pov_fetch_mem_addr;
 
-	(* keep *) `rvformal_rand_reg        pil_mem_valid;
-	(* keep *) `rvformal_rand_reg        pil_mem_ack;
+	(* keep *) `rvformal_rand_reg         pil_mem_valid;
+	(* keep *) `rvformal_rand_reg         pil_mem_ack;
 	// (* keep *) wire                      pil_mem_valid;
 	// (* keep *) wire                      pil_mem_ack;
-	(* keep *) wire                      pol_mem_req;
-	(* keep *) wire                      pol_mem_wen;
+	(* keep *) wire                       pol_mem_req;
+	(* keep *) wire                       pol_mem_wen;
 
-	(* keep *) `rvformal_rand_reg [31:0] piv_mem_rdata;
-	(* keep *) wire               [31:0] pov_mem_wdata;
-	(* keep *) wire               [31:0] pov_mem_addr;
-	(* keep *) wire               [3:0]  pov_mem_byte_sel;
+	(* keep *) `rvformal_rand_reg [31:0]  piv_mem_rdata;
+	(* keep *) wire               [31:0]  pov_mem_wdata;
+	(* keep *) wire               [31:0]  pov_mem_addr;
+	(* keep *) wire               [3:0]   pov_mem_byte_sel;
 
-	(* keep *) `rvformal_rand_reg        pil_soft_irq;
-	(* keep *) `rvformal_rand_reg        pil_timer_irq;
-	(* keep *) `rvformal_rand_reg        pil_ext_irq;
-    (* keep *) wire                      pol_irq_pending;
-
-	(* keep *) wire [6:0]   pitr_inst_v_opcode;
-	(* keep *) wire [11:7]  pitr_inst_v_reg_rd;
-	(* keep *) wire [14:12] pitr_inst_v_funct3;
-	(* keep *) wire [19:15] pitr_inst_v_reg_rs1;
-	(* keep *) wire [24:20] pitr_inst_v_reg_rs2;
-	(* keep *) wire [31:25] pitr_inst_v_funct7;
-
-	assign pitr_inst_v_opcode  = piv_inst[6:0];
-	assign pitr_inst_v_reg_rd  = piv_inst[11:7];
-	assign pitr_inst_v_funct3  = piv_inst[14:12];
-	assign pitr_inst_v_reg_rs1 = piv_inst[19:15];
-	assign pitr_inst_v_reg_rs2 = piv_inst[24:20];
-	assign pitr_inst_v_funct7  = piv_inst[31:25];
+	(* keep *) `rvformal_rand_reg         pil_soft_irq;
+	(* keep *) `rvformal_rand_reg         pil_timer_irq;
+	(* keep *) `rvformal_rand_reg         pil_ext_irq;
+    (* keep *) wire                       pol_irq_pending;
 
 	svx32_core uut (
-		.pil_clk		      (clock               ),
-        .pil_rst		      (reset               ),
-        .pil_run_prg          (1'b1                ),
-        .pitr_inst_v_opcode   (pitr_inst_v_opcode  ),
-        .pitr_inst_v_reg_rd   (pitr_inst_v_reg_rd  ),
-        .pitr_inst_v_funct3   (pitr_inst_v_funct3  ),
-        .pitr_inst_v_reg_rs1  (pitr_inst_v_reg_rs1 ),
-        .pitr_inst_v_reg_rs2  (pitr_inst_v_reg_rs2 ),
-        .pitr_inst_v_funct7   (pitr_inst_v_funct7  ),
-        .pov_addr             (pov_addr            ),
+		.pil_clk		      (clock                 ),
+        .pil_rst		      (reset                 ),
+        .pil_run_prg          (1'b1                  ),
 
-        // --- mem unit signals ---
-        .pil_mem_valid        (pil_mem_valid       ),
-        .pil_mem_ack          (pil_mem_ack         ),
-        .pol_mem_req          (pol_mem_req         ),
-        .pol_mem_wen          (pol_mem_wen         ),
+		// --- instruction fetch signals --- //
+		.pil_fetch_mem_valid  (pil_fetch_mem_valid   ),
+		.pil_fetch_mem_ack    (pil_fetch_mem_ack     ),
+		.pol_fetch_mem_req    (pol_fetch_mem_req     ),
+		.piv_fetch_mem_rdata  (piv_fetch_mem_rdata   ),
+		.pov_fetch_mem_addr   (pov_fetch_mem_addr    ),
 
-        .piv_mem_rdata        (piv_mem_rdata       ),
-        .pov_mem_wdata        (pov_mem_wdata       ),
-        .pov_mem_addr         (pov_mem_addr        ),
-        .pov_mem_byte_sel     (pov_mem_byte_sel    ),
+        // --- mem unit signals --- //
+        .pil_mem_valid        (pil_mem_valid         ),
+        .pil_mem_ack          (pil_mem_ack           ),
+        .pol_mem_req          (pol_mem_req           ),
+        .pol_mem_wen          (pol_mem_wen           ),
 
-		// --- IRQs ---
-		.pil_soft_irq         (pil_soft_irq        ),
-		.pil_timer_irq        (pil_timer_irq       ),
-		.pil_ext_irq          (pil_ext_irq         ),
-		.pol_irq_pending      (pol_irq_pending     ),
+        .piv_mem_rdata        (piv_mem_rdata         ),
+        .pov_mem_wdata        (pov_mem_wdata         ),
+        .pov_mem_addr         (pov_mem_addr          ),
+        .pov_mem_byte_sel     (pov_mem_byte_sel      ),
 
-        // --- risc-v formal interface ---
+		// --- IRQs --- //
+		.pil_soft_irq         (pil_soft_irq          ),
+		.pil_timer_irq        (pil_timer_irq         ),
+		.pil_ext_irq          (pil_ext_irq           ),
+		.pol_irq_pending      (pol_irq_pending       ),
+
+        // --- risc-v formal interface --- //
 		`RVFI_CONN
 	);
 
@@ -89,6 +81,19 @@ module rvfi_wrapper (
 	// 		pil_mem_valid <= 0;
 	// 	end
 	// end
+
+	// Restrict fetch delay //
+	always @(posedge clock) begin
+		pil_fetch_mem_ack <= pol_fetch_mem_req;
+	end
+
+	always @(posedge clock) begin
+		if (pil_fetch_mem_ack) begin
+			pil_fetch_mem_valid <= 1;
+		end else begin
+			pil_fetch_mem_valid <= 0;
+		end
+	end
 
 `ifdef SPARROWX32_FAIRNESS
 	(* keep *) reg [2:0] data_req_pending_cycles = 0;
